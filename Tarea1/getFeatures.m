f509c18ec17ec1ec44716be40ccd4fbd9f784751
farodrig <farodrig@dcc.uchile.cl>
2014-08-16 22:11:58 -0400

    Agregado Main y auxiliares
    Se agregó el main para correr el programa, no estoy seguro si funciona,
    porque no tenemos las funciones que faltan para probarlo. Modifiqué
    loadImg para que se cargaran todas las imágenes y solo se necesite poner
    el tipo de segundo argumento; retorna las imagenes y un vector con el
    número al que corresponde dicha imagen.
    getFeatures crea las matrices de caracteristicas y las guarda, si ya
    estan creadas, solo carga los datos, sin tener que cargar todas las
    imagenes y procesarlas; retorna las matrices y el vector con el numero
    correspondiente de las imagenes.

---
+++ Tarea1/getFeatures.m
@@ -0,0 +1,39 @@
+function [trece, cuatro, ocho, number] = getFeatures(g)
+    %si alguno de los archivos no existe, carga las imagenes porque tendrá
+    %que calcular alguna matriz de características.
+    if(exist('features13.mat', 'file')~=2 || exist('features4.mat', 'file')~=2 || exist('features13.mat', 'file')==2)
+        [train, number] = loadImg('digitos\train\digit_0\', 'png');
+    end
+    %Si existe un archivo con la matriz de características la carga, sino,
+    %la crea y la guarda.
+    if(exist('features13.mat', 'file')==2)
+        trece = load('features13.mat').trece;
+    else
+        trece = zeros(numel(train), 13);
+        for i = 1:numel(train)
+            trece(i,:) = bins13(train(i).img, g, g);
+        end
+        save('features13.mat', 'trece');
+    end
+
+    if(exist('features4.mat', 'file')==2)
+        cuatro = load('features4.mat').cuatro;
+    else
+        cuatro = zeros(numel(train), 16);
+        for i = 1:numel(train)
+            cuatro(i,:) = cc4(train(i).img, g, g);
+        end
+        save('features4.mat', 'cuatro');
+    end
+
+    if(exist('features8.mat', 'file')==2)
+        ocho = load('features8.mat').ocho;
+    else
+        ocho = zeros(numel(train), 16);
+        for i = 1:numel(train)
+            ocho(i,:) = cc8(train(i).img, g, g);
+        end
+        save('features8.mat', 'ocho');
+    end
+end
+
--- Tarea1/loadImg.m
+++ Tarea1/loadImg.m
@@ -1,10 +1,20 @@
-%recibe el path del directoria y el tipo de imagen y retorna una
+%recibe el path del directorio y el tipo de imagen y retorna una
 %matriz con todas las imagenes de ese directorio binarizadas
-function images = loadImg(Folder,ImgType)
-   imgDir=dir([Folder '/' ImgType]);
+function [images, class] = loadImg(Folder,ImgType)
+class = [];
+images = [];
+for i = [0,1,2,3,4,5,6,7,8,9]
+   Folder(numel(Folder)-1) = num2str(i);
+   imgDir=dir([Folder '\*.' ImgType]);
    nfile=max(size(imgDir));
-   for i=1:nfile
-    aux = imread([Folder imgDir(i).name]);
-    images(i).img =im2bw(aux,0.3);
+   num = i;
+   for j=1:nfile
+    type(j)=num;
+    aux = imread([Folder imgDir(j).name]);
+    imag(j).img =im2bw(aux,0.3);
    end
+   class = [class type];
+   images = [images imag];
+end
+class = class';
 end
