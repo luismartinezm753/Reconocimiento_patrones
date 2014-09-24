7a7a27f7cfd6e2437f88a658def32c3453a5bd1a
Luism <luis.martinez@ug.uchile.cl>
2014-09-24 18:44:36 -0300

    Resultados para 0.8

--- Tarea2/main.m
+++ Tarea2/main.m
@@ -2,7 +2,7 @@
 %revisar que en linux los 
 img=LoadImg('DataSet/Images/','*.jpg');%data
 gt=LoadImg('DataSet/Ground_Truth/','*.png');%ground truth
-path='DataSet/Results/';
+path='DataSet/Results/R08';
 results=cell(1,numel(img));%imagenes resultado
 porcentaje=zeros(1,numel(img));%pocentaje de igualdad entre el resultado y el ground_truth
 gtPixels = [];
@@ -20,4 +20,4 @@ for i=1:length(img)
     porcentaje(i)=cmpImg(gt{i},results{i});
 end
 plotroc(gtPixels, probPixels);
-save('porcentaje.mat','porcentaje');
+save('porcentaje08.mat','porcentaje');
